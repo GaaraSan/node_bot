caad61257d0bad07047404933412e8ac50dbcfe3
GaaraSan <prosto.artem158@gmail.com>
2022-09-22 17:35:55 +0300

    require dotenv and created store

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
-const Telegraf = require("telegraf").Telegraf,
-    os = require('node:os'),
-    BOT_TOKEN = "5738356050:AAEfmYGhWUJKYl-iVb3vgcyuw0Om3qABdXw";
+require('dotenv').config()
+const Telegraf = require("telegraf").Telegraf
 
-const bot = new Telegraf(BOT_TOKEN);
+const bot = new Telegraf(process.env.BOT_TOKEN);
+let store = {};
+fetch("https://russianwarship.rip/api/v1/statistics/latest",
+        {
+            method: "get",
+            headers: { 'Content-Type': 'application/json' }
+        })
+        .then(response => response.json())
+        .then(data => {
+            store = data;
+        })
 
 bot.start(ctx => {
-    // fetch("https://russianwarship.rip/api/v1/statistics/latest",
-    //     {
-    //         method:"get",
-    //         headers:{'Content-Type': 'application/json'}
-    //     })
-    // .then(response => response.json())
-    // .then(data => {
-    // ctx.reply(message + ": " + data.data.stats[message])});
     ctx.reply("Вітаю, шановне панство!)")
 })
 
 bot.hears(/Привіт+/i, ctx => {
-    ctx.reply("Файно балакаєш, мені це до вподоби"+ "\u{1F970}")
+    ctx.reply("Файно балакаєш, мені це до вподоби" + "\u{1F970}")
 })
 
 bot.hears(/[A-Z]+/i, (ctx) => {
     message = ctx.message.text;
-    fetch("https://russianwarship.rip/api/v1/statistics/latest",
-        {
-            method:"get",
-            headers:{'Content-Type': 'application/json'}
-        })
-    .then(response => response.json())
-    .then(data => {
-        ctx.reply(message + ": " + data.data.stats[message])})}    
+    ctx.reply(message + ": " + store.data.stats[message])
+}
 );
 // });
 
